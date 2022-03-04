92e76060dfba8c69127b715f4503650f86c98f17
Achmad Yusuf Al Ma'ruf <73216195+yusufmaruf@users.noreply.github.com>
2022-03-04 10:16:23 +0700

    Add files via upload

---
+++ latihan js/concat.js
@@ -0,0 +1,7 @@
+function ubah() {
+    var kata = "Saya";
+    var kata1 = " Maruf";
+    console.log(kata.concat(kata1));
+}
+ubah()
+    //digunkan untuk menyambung string yang fungsinya sama dengan operator +
