05174f580f80008d315bfb87c1e9a2ff0b724636
Achmad Yusuf Al Ma'ruf <73216195+yusufmaruf@users.noreply.github.com>
2022-03-05 22:29:08 +0700

    Add files via upload

---
+++ latihan js/starend.js
@@ -0,0 +1,7 @@
+function starend() {
+    var kata = "Saya pintar memasak nasi goreng";
+    console.log(kata.startsWith("Saya"));
+    console.log(kata.endsWith('goreng'));
+}
+starend()
+    //mengecek apakah awaln dan akhir string sesuai dengan kalimat string
