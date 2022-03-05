fc2b09ae235c7dcd4e5f2566dc2318a61305f81d
Achmad Yusuf Al Ma'ruf <73216195+yusufmaruf@users.noreply.github.com>
2022-03-05 22:41:54 +0700

    Add files via upload

---
+++ latihan js/convertstring.js
@@ -0,0 +1,16 @@
+function konversi() {
+    var kata = new String("Belajar kuy...");
+    console.log(typeof kata);
+    console.log(kata)
+    var konf = kata.toString();
+    console.log(typeof konf);
+    console.log(konf)
+    var konf2 = kata.valueOf();
+    console.log(typeof konf2);
+    console.log(typeof konf2);
+
+
+
+
+}
+konversi();
