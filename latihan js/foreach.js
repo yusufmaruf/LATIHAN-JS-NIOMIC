a2def522a2fef4445e9fe6aa21b302c067499ab0
Achmad Yusuf Al Ma'ruf <73216195+yusufmaruf@users.noreply.github.com>
2022-03-07 22:12:43 +0700

    Add files via upload

---
+++ latihan js/foreach.js
@@ -0,0 +1,9 @@
+function feach() {
+    var data = ['a', 'b', 'c'];
+    data.forEach(function(item, index, array) {
+        console.log(item)
+        console.log(index)
+        console.log(array)
+    })
+}
+feach();
---
+++ latihan js/tugas22.js
@@ -0,0 +1,7 @@
+function feach() {
+    var data = ['saya', 'ingin', 'belajar', 'bersama'];
+    data.forEach(function(item, index) {
+        console.log('item : ' + item + " index ke " + index)
+    })
+}
+feach();
