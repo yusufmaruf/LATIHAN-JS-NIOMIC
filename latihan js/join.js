6a980f25aadfcbaab07da130b490e8b6958657ed
Achmad Yusuf Al Ma'ruf <73216195+yusufmaruf@users.noreply.github.com>
2022-03-07 21:16:12 +0700

    Add files via upload

---
+++ latihan js/join.js
@@ -0,0 +1,9 @@
+function jjoin() {
+    var buah = ['apel', 'melon', 'ceri']
+    console.log(buah)
+    var result = buah.join(", ")
+    console.log(result)
+
+}
+jjoin();
+//merubah array menjadi string
